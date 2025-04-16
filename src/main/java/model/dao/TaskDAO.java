412e14e6f0d1366faf7da796d5f9ba1e690d2c97
comsize-araki <ran.araki@comsize.com>
2025-04-16 17:57:10 +0900

    selectStatusの修正

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 import model.entity.CategoryBean;
 import model.entity.StatusBean;
+import model.entity.TaskBean;
 
 public class TaskDAO {
 
 	//ステータスマスタのステータスコードとステータス名をリストで取ってくるメソッド
 	public List<StatusBean> selectStatus() throws ClassNotFoundException, SQLException {
 
-		String sql = "SELECT status_code,status_name FROM m_status";
+		String sql = "SELECT status_code,status_name FROM m_status ORDER BY status_code";
 		List<StatusBean> list = new ArrayList<>();
 
 		try (Connection con = ConnectionManager.getConnection();
@@ -60,4 +61,26 @@ public class TaskDAO {
 		}
 	}
 
+	public int register(TaskBean bean) throws ClassNotFoundException, SQLException {
+
+		String sql = "INSERT INTO t_task (task_name,category_id,limit_date,user_id,status_code,memo) VALUES (?,?,?,?,?,?)";
+		int count = 0;
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			pstmt.setString(1, bean.getTaskName());
+			pstmt.setInt(2, bean.getCategoryID());
+			pstmt.setDate(3,java.sql.Date.valueOf(bean.getLimitDate()));
+			pstmt.setString(4, bean.getUserID());
+			pstmt.setString(5, bean.getStatusCode());
+			pstmt.setString(6, bean.getMemo());
+
+			count = pstmt.executeUpdate();
+		}
+
+		return count;
+
+	}
+	
 }
