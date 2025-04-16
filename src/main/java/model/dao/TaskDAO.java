af1a3a35b87c22e92312e6e0c37057180066dd6d
comsize-araki <ran.araki@comsize.com>
2025-04-16 15:46:17 +0900

    selectStatusメソッドの追加

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -1,5 +1,41 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.StatusBean;
+
 public class TaskDAO {
+
+	public List<StatusBean> selectStatus() throws ClassNotFoundException, SQLException {
+
+		String sql = "SELECT status_code,status_name FROM m_status";
+		List<StatusBean> list = new ArrayList<>();
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			ResultSet res = pstmt.executeQuery();
+
+			while (res.next()) {
+
+				StatusBean bean = new StatusBean();
+
+				bean.setStatusCode(res.getString("status_code"));
+				bean.setStatusName(res.getString("status_name"));
+
+				list.add(bean);
+
+			}
+
+		}
+
+		return list;
+
+	}
 
 }
