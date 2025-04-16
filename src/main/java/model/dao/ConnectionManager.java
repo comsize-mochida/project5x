7228be786841561b66732daba13f250451031f88
comsize-araki <ran.araki@comsize.com>
2025-04-16 15:16:14 +0900

    ConnectionManager作成

---
+++ src/main/java/model/dao/ConnectionManager.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/task_db";
+	private static final String USER = "root";
+	private static final String PASSWORD = "mysql";
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		return DriverManager.getConnection(URL,USER,PASSWORD);
+		
+	}
+	
+}
