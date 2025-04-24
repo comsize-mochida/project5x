0662faa11e3c74303cc336bc5d0afccdf45aa0f6
comsize-araki <ran.araki@comsize.com>
2025-04-24 11:17:08 +0900

    テストの修正

--- test/model/dao/CommentDAOTest.java
+++ test/model/dao/CommentDAOTest.java
@@ -13,6 +13,96 @@ import model.entity.CommentBean;
 class CommentDAOTest {
 
 	@Test
+	void register_成功() {
+		//準備Arrange
+		CommentDAO dao = new CommentDAO();
+		int count = 0;
+		CommentBean bean = new CommentBean();
+		bean.setTaskID(1);
+		bean.setUserID("a-takahashi");
+		bean.setComment("サンプル");
+
+		//実行Act
+		try {
+			count = dao.register(bean);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertEquals(1, count);
+	}
+
+	@Test
+	void register_失敗() {
+		//準備Arrange
+		CommentDAO dao = new CommentDAO();
+		int count = 0;
+		CommentBean bean = new CommentBean();
+		bean.setTaskID(100);
+		bean.setUserID("a-takahashi");
+		bean.setComment("サンプル");
+
+		//実行Act
+		try {
+			count = dao.register(bean);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertEquals(0, count);
+	}
+
+	@Test
+	void delete_成功() {
+		//準備Arrange
+		CommentDAO dao = new CommentDAO();
+		int commentID = 2;
+		int count = 0;
+		CommentBean bean = null;
+
+		//実行Act
+		try {
+			count = dao.delete(commentID);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertEquals(1, count);
+
+	}
+
+	@Test
+	void delete_失敗() {
+		//準備Arrange
+		CommentDAO dao = new CommentDAO();
+		int commentID = 100;
+		int count = 0;
+		CommentBean bean = null;
+
+		//実行Act
+		try {
+			count = dao.delete(commentID);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertEquals(0, count);
+
+	}
+
+	@Test
 	void selectAll_成功() {
 
 		CommentDAO dao = new CommentDAO();
--- test/model/dao/TaskDAOTest.java
+++ test/model/dao/TaskDAOTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import model.entity.CategoryBean;
 import model.entity.StatusBean;
 import model.entity.TaskBean;
 
@@ -101,6 +102,99 @@ class TaskDAOTest {
 		//検証
 		assertNotNull(list);
 
+	}
+
+	@Test
+	void update_成功() {
+		//準備Arrange
+		TaskDAO dao = new TaskDAO();
+		int count = 0;
+		TaskBean bean = new TaskBean();
+		bean.setTaskName("サンプル");
+		bean.setCategoryID(1);
+		bean.setLimitDate(LocalDate.parse("2025-04-30"));
+		bean.setUserID("a-takahashi");
+		bean.setStatusCode("00");
+		bean.setMemo("サンプル");
+		bean.setTaskID(1);
+
+		//実行Act
+		try {
+			count = dao.update(bean);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertEquals(1, count);
+	}
+
+	@Test
+	void update_失敗() {
+		//準備Arrange
+		TaskDAO dao = new TaskDAO();
+		int count = 0;
+		TaskBean bean = new TaskBean();
+		bean.setTaskName(null);
+		bean.setCategoryID(1);
+		bean.setLimitDate(LocalDate.parse("2025-04-30"));
+		bean.setUserID("a-takahashi");
+		bean.setStatusCode("00");
+		bean.setMemo("サンプル");
+		bean.setTaskID(1);
+
+		//実行Act
+		try {
+			count = dao.update(bean);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertEquals(0, count);
+	}
+
+	@Test
+	void selectCategory_成功() {
+		//準備Arrange
+		TaskDAO dao = new TaskDAO();
+		List<CategoryBean> list = new ArrayList<>();
+
+		//実行Act
+		try {
+			list = dao.selectCategory();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertNotNull(list);
+	}
+
+	@Test
+	void selectTask_成功() {
+		//準備Arrange
+		TaskDAO dao = new TaskDAO();
+		TaskBean bean = null;
+		int taskID = 1;
+
+		//実行Act
+		try {
+			bean = dao.selectTask(taskID);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		//検証Assert
+		assertNotNull(bean);
 	}
 
 }
