c916a29e0ce8c0b3997c376c182edccdaa4487da
comsize-yuki <yukiroireine@gmail.com>
2025-04-24 11:27:23 +0900

    ログイン機能のテストクラス作成

--- test/model/dao/UserDAOTest.java
+++ test/model/dao/UserDAOTest.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,5 +33,39 @@ class UserDAOTest {
 		}
 		
 	}
+	@Test
+	void login_成功() {
+		UserDAO dao = new UserDAO();
+		UserBean userBean = null;
+		String userID = "a-takahashi";
+		String password = "ai";
+		try {
+			userBean = dao.login(userID, password);
+		}catch(ClassNotFoundException | SQLException e){
+			e.printStackTrace();
+		}
+		assertEquals("高橋愛", userBean.getUserName());
+	}
+	@Test
+	void login_失敗() {
+		//		準備
+		UserDAO dao = new UserDAO();
+		UserBean userBean = null;
+		String 	userID = "tarou";
+		String password = "test";
+
+		//		実行
+		try {
+			userBean = dao.login(userID, password);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		//	検証
+		assertNull(userBean.getUserName());
+	}
+
+
 
 }
